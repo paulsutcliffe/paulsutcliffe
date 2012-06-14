b9fc0392e5bd0c5ebd88cddc6e8887da167c4f16
paulyoyo <paul@kosmyka.com>
2012-06-14 03:02:05 -0300

    fixed capistrano stuff, display 6 gigs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @slides = Slide.all
-    @gigs = Gig.order(:date).paginate(:page => params[:page], :per_page => 4)
+    @gigs = Gig.order(:date).paginate(:page => params[:page], :per_page => 6)
     @scwidgets = Scwidget.all
   end
 
