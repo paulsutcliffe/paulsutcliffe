7b112191c29104aa8dfa5d4531ad68c1a3a629ab
paulyoyo <paul@kosmyka.com>
2012-06-14 01:36:37 -0300

    fixing cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,8 @@ set :branch, "master"
 set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
 set :scm_verbose, true
+set :deploy_to, applicationdir
+
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db,  domain, :primary => true # This is where Rails migrations will run
