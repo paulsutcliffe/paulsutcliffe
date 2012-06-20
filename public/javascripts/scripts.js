b817f0d205b79dc5535dbc8b8430da02d2735698
Ricardo <marcorcp1@hotmail.com>
2012-06-20 03:01:03 -0500

    Replaced royal slider with scroller, added many js files

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -21,5 +21,82 @@ $('header .social li#rss a').text('');
 $('.social li#rss a').text('');
 
 
+
+
+
+
+
+
+	
+		                                        
+/*
+=================================================================
+SLIDER
+=================================================================
+*/
+	
+	function starScrolling(){
+		$("div#makeMeScrollable").smoothDivScroll({ 
+			autoScroll: "onstart" , 
+			autoScrollDirection: "backandforth", 
+			autoScrollStep: 1, 
+			autoScrollInterval: 15,	
+			startAtElementId: "startAtMe",
+			visibleHotSpots: "always"
+		});
+	};
+
+	// centers the scroll on resize
+	
+	function centerScroll(){
+		var h = $(window).height()/2 - $('#scroll-holder').height()/2;
+		$('#scroll-holder').css({
+			top: function(index, value){ return h }
+		});
+	}
+
+	
+	//##########################################
+	// On load page
+	//##########################################
+
+	$(window).load(function(){
+		// show scroller
+		$('#scroll-holder').fadeIn();
+	
+		starScrolling();
+		
+		// Center and resize after all page is loaded
+		centerScroll();
+	
+		
+				
+	});
+	
+
+
+
+/*
+<%= stylesheet_link_tag  '/javascripts/libs/SmoothDivScroll/css/smoothDivScroll.css' %>  ---- dependiente
+<%= javascript_include_tag '/javascripts/libs/fancybox/lib/jquery.mousewheel-3.0.6.pack.js', 
+'/javascripts/libs/fancybox/jquery.fancybox.js',    --- dependiente
+'/javascripts/libs/fancybox/helpers/jquery.fancybox-buttons.js?v=2.0.4', 
+'/javascripts/libs/fancybox/helpers/jquery.fancybox-thumbs.js?v=2.0.4', 
+'/javascripts/libs/SmoothDivScroll/js/jquery.smoothDivScroll-1.1.js'  -- dependiente
+
+
+
+
+
+
+
+*/
+
+
+
+
+
 }) // END DOCUMENT READY
+
+
 
