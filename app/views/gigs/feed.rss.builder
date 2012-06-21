0184d8e5dcdb73f567e274daa9c523bc7614931e
paulyoyo <paul@kosmyka.com>
2012-06-20 19:50:12 -0500

    Fixing capistrano and cleaning routes.

--- app/views/gigs/feed.rss.builder
+++ app/views/gigs/feed.rss.builder
@@ -1,7 +1,7 @@
 xml.rss :version=>"2.0" do
   xml.channel do
     xml.title "Paul Sutcliffe's Gigs"
-    xml.link "http://localhost:3000"
+    xml.link "http://www.djpaulsutcliffe.com"
     xml.description "Dj Paul Sutcliffe's Gigs"
     xml.language 'pt-br'
     @gigs.each do |gig|
--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,6 @@ require "bundler/capistrano"
 default_environment['PATH']='/usr/lib/ruby/gems/1.8/bin:/home/paulsutcliffe/.gems/bin:/usr/local/bin:/usr/bin:/bin'
 default_environment['GEM_PATH']='/home/paulsutcliffe/.gems:/usr/lib/ruby/gems/1.8'
 
-
 set :user, "paulsutcliffe"
 set :domain, "djpaulsutcliffe.com"
 set :project, "paulsutcliffe"
@@ -15,7 +14,6 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 set :git_enable_submodules, 1
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :scm_passphrase, ""
 set :branch, "master"
 set :deploy_via, :remote_cache
@@ -26,26 +24,11 @@ set :deploy_to, applicationdir
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db,  domain, :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
 set :use_sudo, false
 desc "Fix permissions after code update."
 task :after_update_code, :roles => [:app] do
   run "chmod 775 #{application_dir}/current/public/dispatch.fcgi"
 end
 
-after 'deploy:create_symlink', 'deploy:after_after_update_code'
+after 'deploy:create_symlink', 'deploy:after_update_code'
