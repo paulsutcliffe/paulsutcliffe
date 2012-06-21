8fe77e90793f29930bc22839349b1f543a0c3d22
paulyoyo <paul@kosmyka.com>
2012-06-20 20:39:31 -0500

    capistrano your are making me cry

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,13 +19,14 @@ set :branch, "master"
 set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
 set :scm_verbose, true
-set :deploy_to, :applicationdir
+set :deploy_to, applicationdir
 
 role :web, domain                          # Your HTTP server, Apache/etc
 role :app, domain                          # This may be the same as your `Web` server
 role :db,  domain, :primary => true # This is where Rails migrations will run
 
 set :use_sudo, false
+
 after 'deploy:update_code' do
   run "chmod 775 paulsutcliffe/current/public/dispatch.fcgi"
 end
