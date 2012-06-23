6e4707e200b1efbad2935c027bc659972c19cb73
paulyoyo <paul@kosmyka.com>
2012-06-23 16:33:21 -0500

    Fixed the sliders route to system

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,6 +1,6 @@
 class Slide < ActiveRecord::Base
-  
+
   has_attached_file :slide, :styles => { :min => ["120x450>", :jpg], :max => ["1440x450#", :jpg]  },
-                            :url  => "/images/assets/slides/:id/:style/:basename.:extension",
-                            :path => ":rails_root/public/images/assets/slides/:id/:style/:basename.:extension"
+                            :url  => "/system/assets/slides/:id/:style/:basename.:extension",
+                            :path => ":rails_root/public/system/assets/slides/:id/:style/:basename.:extension"
 end
